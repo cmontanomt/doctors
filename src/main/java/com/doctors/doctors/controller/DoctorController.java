574d32c95dc82861f5bf3a4e8264150bbd914da3
cmontanomt <cesar.montano.mt@usa.edu.co>
2022-10-04 18:50:10 -0500

    first commit

---
+++ src/main/java/com/doctors/doctors/controller/DoctorController.java
@@ -0,0 +1,45 @@
+package com.doctors.doctors.controller;
+
+import com.doctors.doctors.model.DoctorModel;
+import com.doctors.doctors.service.DoctorService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.print.Doc;
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/api/doctor")
+@CrossOrigin(origins = "*")
+public class DoctorController {
+
+    @Autowired
+    private DoctorService doctorService;
+
+    @GetMapping("/all")
+    public List<DoctorModel> getAllDoctors(){
+        return doctorService.getAllDoctors();
+    }
+
+    @GetMapping("/{id}")
+    public Optional<DoctorModel> getDoctor(@PathVariable("id") Integer id){
+        return doctorService.getDoctor(id);
+    }
+
+    @PostMapping("/save")
+    public DoctorModel saveDoctor(@RequestBody DoctorModel doctorModel){
+        return doctorService.saveDoctor(doctorModel);
+    }
+
+    @DeleteMapping("/delete/{id}")
+    public boolean deleteDoctor(@PathVariable("id") Integer id){
+        return  doctorService.deleteDoctor(id);
+    }
+
+    @PutMapping("/update")
+    public DoctorModel updateDoctor(@RequestBody DoctorModel doctorModel){
+        return doctorService.updateDoctor(doctorModel);
+    }
+}
---
+++ src/main/java/com/doctors/doctors/repository/crudrepository/DoctorCrudRepository.java
@@ -0,0 +1,8 @@
+package com.doctors.doctors.repository.crudrepository;
+
+import com.doctors.doctors.model.DoctorModel;
+import org.springframework.data.repository.CrudRepository;
+
+public interface DoctorCrudRepository extends CrudRepository<DoctorModel,Integer> {
+
+}
